ece9cdb995c21ba766ac14ba7fb11fe9ef637fac
Arman <armandwi.pangestu7@gmail.com>
2024-09-02 01:00:50 +0700

    user: fix permission

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -30,7 +30,7 @@ class UserController extends Controller
 			array(
 				'allow',  // allow all users to perform 'index' and 'view' actions
 				'actions' => array('index', 'view'),
-				'users' => array('*'),
+				'roles' => array('user', 'admin'),
 			),
 			// array('allow', // allow authenticated user to perform 'create' and 'update' actions
 			// 	'actions'=>array('create','update'),
