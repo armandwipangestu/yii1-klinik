6083483c0fcf03e5f3e3102bb7233e19a8cfc339
Arman <armandwi.pangestu7@gmail.com>
2024-09-02 01:51:34 +0700

    hotfix: update homepage content

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -8,7 +8,12 @@ $this->pageTitle = Yii::app()->name;
 
 <p>Aplikasi ini adalah hasil Tes Skill Web Programmer GRIT dari PT Inova Medika Solusindo</p>
 
-<p>Aplikasi ini mempunyai kriteria sebagai berikut:</p>
+<p>Deskripsi Test:</p>
+
+<p>Terima kasih sebelumnya telah mengikuti serangkaian proses seleksi di Inova Medika, selanjutnya untuk penilaian pada kompetensi/skill sebagai Programmer, maka kami memberikan tugas rumah untuk test programming kami lakukan online "membuat aplikasi Sistem Informasi Klinik"</p>
+
+<p>Ketentuan aplikasi secara umum yang dibuat terdiri dari :</p>
+
 <ol>
 	<li>Terdapat user login berdasarkan hak aksesnya (SRBAC)</li>
 	<li>Terdapat master CRUD untuk pembuatan master wilayah, user, dan pegawai, tindakan, obat</li>
@@ -17,5 +22,24 @@ $this->pageTitle = Yii::app()->name;
 	<li>Terdapat menu informasi untuk melakukan pembayaran tagihan pasien</li>
 	<li>Terdapat menu laporan yang dapat ditampilkan dengan grafik</li>
 </ol>
+
+<p>Aplikasi yang dibuat menggunakan Framework Yii-1, database PostgresSql atau dengan database lainnya yang dianggap lebih menguasai.</p>
+
+<p>
+	Hasil tes bisa dikirimkan ke email dengan format:
+</p>
+
+<b>Dengan format subject : Nama_Pengumpulan Test Skill_Posisi yg dilamar</b><br />
+ke alamat email ( <a href="mailto:career@inovamedika.com">career@inovamedika.com</a> )<br /><br />
+
+Adapun dokumen yang dikirimkan adalah:
+<ul>
+	<li>screenshoot aplikasi</li>
+	<li>screenshoot table yang dipakai</li>
+	<li>Link hasil pengerjaan ke GitHub repository dan share link nya</li>
+</ul>
+
+PT. Inova Medika Solusindo
+<a href="https://www.inovamedika.com">www.inovamedika.com</a><br /><br />
 
 <p>Aplikasi ini dibuat oleh <a href="https://armandwipangestu.vercel.app" target="_blank">Arman Dwi Pangestu</a>. Source code aplikasi ini tersedia di <a href="https://github.com/armandwipangestu/yii1-klinik" target="_blank">GitHub - yii1-klinik</a></p>
