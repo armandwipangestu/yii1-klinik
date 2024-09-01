9d91a90453c6e30e28726e874ee0b51205888769
Arman <armandwi.pangestu7@gmail.com>
2024-09-01 14:08:19 +0700

    config: add tablePrefix

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,13 +2,12 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// 'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+	'connectionString' => 'mysql:host=localhost;dbname=yii1_klinik',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'user_yii1_klinik',
+	'password' => 'yii',
 	'charset' => 'utf8',
-	*/
+	'tablePrefix' => 'tbl_',
 );
