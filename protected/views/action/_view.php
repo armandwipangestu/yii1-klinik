b53e5b75907d02f761f4cfc0bb56cf5dce548df5
Arman <armandwi.pangestu7@gmail.com>
2024-09-02 10:59:12 +0700

    action: CRUD

--- protected/views/action/_view.php
+++ protected/views/action/_view.php
@@ -6,7 +6,7 @@
 <div class="view">
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id' => $data->id)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
@@ -17,12 +17,12 @@
 	<?php echo CHtml::encode($data->price); ?>
 	<br />
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
+	<!-- <b><?php echo CHtml::encode($data->getAttributeLabel('created_at')); ?>:</b>
 	<?php echo CHtml::encode($data->created_at); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('updated_at')); ?>:</b>
-	<?php echo CHtml::encode($data->updated_at); ?>
+	<?php echo CHtml::encode($data->updated_at); ?> -->
 	<br />
 
 
